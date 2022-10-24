249b9a119143c26730e7c7c1d792a10b62a7702d
freetimezp <kraftukrnet@ukr.net>
2022-10-24 20:42:28 +0300

    06. js-e-commerce-03. Product-box component.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+const ROWS_HEIGHT: { [id: number]: number } = {
+  1: 400, 3: 335, 4: 350
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: `./home.component.html`,
 })
 export class HomeComponent implements OnInit {
   cols = 3;
+  rowHeight = ROWS_HEIGHT[this.cols];
+  category: string | undefined;
 
   constructor() { }
 
@@ -14,6 +20,11 @@ export class HomeComponent implements OnInit {
 
   onColumnsCountChamge(colsNum: number): void {
     this.cols = colsNum;
+    this.rowHeight = ROWS_HEIGHT[this.cols];
+  }
+
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
   }
 
 }
